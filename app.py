34ff9e18c4d3b3f1ba61e8febc2bea1e665b16a6
Terry Green <greentd@gmail.com>
2025-01-21 02:31:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import os
-import tensor
 from dotenv import find_dotenv, load_dotenv
-from transformers import pipeline
+from huggingface_hub import InferenceClient
 
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
-# API_KEY = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+API_KEY = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+client = InferenceClient(api_key=API_KEY)
+
+messages = [
+	{
+		"role": "user",
+		"content": [
+			{
+				"type": "text",
+				"text": "Describe this image in one sentence."
+			},
+			{
+				"type": "image_url",
+				"image_url": {
+					"url": "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
+				}
+			}
+		]
+	}
+]
 
 # img2text
-def img2text(url):
-    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+completion = client.chat.completions.create(
+    model="meta-llama/Llama-3.2-11B-Vision-Instruct", 
+	messages=messages, 
+	max_tokens=500
+)
 
-    text = image_to_text(url)[0]["generated_text"]
+print(completion.choices[0].message)
 
-    print(text)
-    return text
-
-img2text("test-image.png")
 
 # llm
-# https://huggingface.co/spaces/KwaiVGI/LivePortrait
+
 
 # text to speech
